ec49b4a7f0121513ae861f57d7a55634bf6a2d88
Luis Valdivia <lcarlos.mairena23@gmail.com>
2021-11-23 23:46:43 -0600

    Se realiza ajuste para mostrar bordes redondeados

--- rounded-corners.js
+++ rounded-corners.js
@@ -23,7 +23,7 @@
 
         proceed.call(this);
 
-        this.points.forEach(function (point) {
+        H.each(this.points, function (point) {
             var shapeArgs = point.shapeArgs,
                 w = shapeArgs.width,
                 h = shapeArgs.height,
@@ -37,7 +37,7 @@
                 rBottomLeft = rel(options.borderRadiusBottomLeft || 0, w);
         
             if (rTopLeft || rTopRight || rBottomRight || rBottomLeft) {
-
+                var maxR = Math.min(w, h) / 2
                 if (options.stacking === "normal") {
                     if (point.stackY == point.stackTotal) {
                         rBottomRight = 0;
@@ -49,26 +49,24 @@
                         rTopRight = 0;
                         rTopLeft = 0;
                     }
-                }
-                else {
-                    var maxR = Math.min(w, h) / 2
+                } else { 
                     
-                    if (rTopLeft > maxR) {
-                        rTopLeft = maxR;
-                    }
-
-                    if (rTopRight > maxR) {
-                        rTopRight = maxR;
-                    }
-
-                    if (rBottomRight > maxR) {
-                        rBottomRight = maxR;
-                    }
-
-                    if (rBottomLeft > maxR) {
-                        rBottomLeft = maxR;
-                    }
+                if (rTopLeft > maxR) {
+                    rTopLeft = maxR;
                 }
+
+                if (rTopRight > maxR) {
+                    rTopRight = maxR;
+                }
+
+                if (rBottomRight > maxR) {
+                    rBottomRight = maxR;
+                }
+
+                if (rBottomLeft > maxR) {
+                    rBottomLeft = maxR;
+                }
+            }
 
                 // Preserve the box for data labels
                 point.dlBox = point.shapeArgs;
@@ -76,24 +74,24 @@
                 point.shapeType = 'path';
                 point.shapeArgs = {
                     d: [
-                        ['M', x + rTopLeft, y + topMargin],
+                        'M', x + rTopLeft, y + topMargin,
                         // top side
-                        ['L', x + w - rTopRight, y + topMargin],
+                        'L', x + w - rTopRight, y + topMargin,
                         // top right corner
-                        ['C', x + w - rTopRight / 2, y, x + w, y + rTopRight / 2, x + w, y + rTopRight],
+                        'C', x + w - rTopRight / 2, y, x + w, y + rTopRight / 2, x + w, y + rTopRight,
                         // right side
-                        ['L', x + w, y + h - rBottomRight],
+                        'L', x + w, y + h - rBottomRight,
                         // bottom right corner
-                        ['C', x + w, y + h - rBottomRight / 2, x + w - rBottomRight / 2, y + h, x + w - rBottomRight, y + h + bottomMargin],
+                        'C', x + w, y + h - rBottomRight / 2, x + w - rBottomRight / 2, y + h, x + w - rBottomRight, y + h + bottomMargin,
                         // bottom side
-                        ['L', x + rBottomLeft, y + h + bottomMargin],
+                        'L', x + rBottomLeft, y + h + bottomMargin,
                         // bottom left corner
-                        ['C', x + rBottomLeft / 2, y + h, x, y + h - rBottomLeft / 2, x, y + h - rBottomLeft],
+                        'C', x + rBottomLeft / 2, y + h, x, y + h - rBottomLeft / 2, x, y + h - rBottomLeft,
                         // left side
-                        ['L', x, y + rTopLeft],
+                        'L', x, y + rTopLeft,
                         // top left corner
-                        ['C', x, y + rTopLeft / 2, x + rTopLeft / 2, y, x + rTopLeft, y],
-                        ['Z']
+                        'C', x, y + rTopLeft / 2, x + rTopLeft / 2, y, x + rTopLeft, y,
+                        'Z'
                     ]
                 };
             }
